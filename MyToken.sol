5420d687ce0b2c56de93d9215a489e2b1c741a54
dinesh-aamous <dineshaamous@gmail.com>
2024-06-10 15:56:38 +0530

    Update MyToken.sol

--- MyToken.sol
+++ MyToken.sol
@@ -23,3 +23,29 @@ contract exp {
     }
 
 }
+
+// task 2
+
+// SPDX-License-Identifier: MIT
+
+pragma solidity >0.8.0 <0.9.0;
+
+contract exp {
+
+    function exp1(uint age, uint salary) public pure returns (uint) {
+        
+        if (age > 30) {
+            return salary + 25000;
+        }
+        else if (age > 20) {
+            return salary + 3000;
+        }
+        else if (age > 18) {
+            return salary + 2000;
+        }
+        else {
+            return salary;
+        }
+    }
+
+}
