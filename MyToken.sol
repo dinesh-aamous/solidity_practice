358d04e7b5eeebe9c9d3c0ab0bba230f73c58f65
dinesh-aamous <dineshaamous@gmail.com>
2024-06-15 14:41:09 +0530

    Update MyToken.sol

--- MyToken.sol
+++ MyToken.sol
@@ -49,3 +49,82 @@ contract exp {
     }
 
 }
+
+// task 3
+
+contract loop{
+    uint[] public numbers;
+    uint public sum;
+    uint public  dosum;
+
+    function initial( uint count) public{
+        for(uint i=0; i<count; i++){
+            numbers.push(i);
+        }
+    }
+
+    function sumwhile() public {
+        uint i=0;
+        sum = 0;
+        while( i<numbers.length){
+            sum+=numbers[i];
+            i++;
+        }
+    }
+
+    function sumdo() public view returns (uint) {
+        uint i=0;
+        uint dosum = 1;
+        if(numbers.length>0){
+            do{
+                dosum*=numbers[i]+1;
+                i++;
+            }
+            while(i<numbers.length);
+        }
+        return dosum;
+    }
+}
+
+// task 4
+
+contract array {
+
+    uint [5] fixedsizearray;
+    function populatearray () public {
+        for (uint i=0; i<5; i++){
+            fixedsizearray [i] =i+1;
+        }
+    }
+    function getarray() public view returns (uint [5] memory){
+        return fixedsizearray;
+    }
+    uint[] dynamic;
+    function add(uint value) public {
+        dynamic.push(value);
+    }
+    function get() public view returns (uint[] memory) {
+        return dynamic;
+    }
+    function populate () public {
+        dynamic.pop();
+    }
+}
+
+// task 5
+
+contract Ledger {
+    mapping (address => uint) public balances;
+    function updateBalance (uint newbalance) public {
+        balances[msg.sender] = newbalance;
+    }
+
+}
+
+contract Updater{
+    function updatebalance() public returns(uint){
+        Ledger ledgerbalance = new Ledger();
+        ledgerbalance.updateBalance(10);
+        return ledgerbalance.balances(address(this));
+    }
+}
